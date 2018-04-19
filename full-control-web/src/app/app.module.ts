4c558c6760f29cc8b914e0bd26fea96db64bf497
Samuel Silva <Samuel.Silva@csgi.com>
2018-04-19 16:12:30 -0300

    added layout base structure

--- full-control-web/src/app/app.module.ts
+++ full-control-web/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MonitorComponent } from './monitor/monitor.component';
+import { ManagerComponent } from './manager/manager.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonitorComponent,
+    ManagerComponent
   ],
   imports: [
     BrowserModule
